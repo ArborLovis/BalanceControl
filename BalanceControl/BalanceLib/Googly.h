8076a549f5bb47cf881ea30a8cc5926e35267773
waldhube16 <simon.waldhuber@edu.fh-joanneum.at>
2018-05-30 11:52:19 +0200

    Added attributes and methods to class Googly

--- BalanceControl/BalanceLib/Googly.h
+++ BalanceControl/BalanceLib/Googly.h
@@ -2,7 +2,20 @@
 class Googly
 {
 public:
-	Googly();
+	Googly(float radius, float position, float velocity);
+
+	void set_velocity(float velocity);
+	float get_velocity() const;
+	void set_position(float position);
+	float get_position() const;
+
 	~Googly();
+
+private:
+	//float colour_;  //for later use
+	float radius_;
+	float position_;
+	float velocity_;
+	
 };
 
