92401146a990a5aee47b9efad7886e3f6cf65d8a
Ralph Berghofer <ralph.berghofer@edu.fh-joanneum.at>
2018-06-06 14:36:11 +0200

    changed Rocker Files

--- BalanceControl/BalanceLib/Rocker.h
+++ BalanceControl/BalanceLib/Rocker.h
@@ -1,22 +1,29 @@
 #pragma once
+
+#include <chrono>
+
 class Rocker
 {
+	using time_type = std::chrono::time_point<std::chrono::system_clock>;
+
 public:
 
-	Rocker(float lenght, float act_angle, float min_angle, float max_angle);
+	Rocker(float lenght, float act_angle);
 
-	float get_angle() const;
+	float get_angle(const float delta_time_mico);
 	void set_angle(float act_angle);
-
-	~Rocker();
-
+	
 private:
 
-	float length_;
-	float act_angle_;
-	float min_angle_;
-	float max_angle_;
-	float max_ang_speed_;
+	const float PI = 3.141592;
 
+	float length_;
+	float target_angle_;
+	float act_angle_;
+	float min_angle_ = -(PI / 8);
+	float max_angle_ = (PI / 8);
+	const float max_ang_speed_micro_ = (PI / 4) / 1000000;  //per microsecond
+	
+	void angle_change(const float delta_time_micro);
 };
 
