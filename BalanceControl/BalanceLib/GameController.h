2758045715ff016ad3e3e0f06abf52eccce9489d
waldhube16 <simon.waldhuber@edu.fh-joanneum.at>
2018-06-06 14:55:00 +0200

    changes to GameController

--- BalanceControl/BalanceLib/GameController.h
+++ BalanceControl/BalanceLib/GameController.h
@@ -6,7 +6,7 @@
 #include "Stabilizer.h"
 #include <chrono>
 
-const float PI = 3.141592;
+
 using sys_now_duration = std::chrono::duration<long long, std::ratio<1, 1000000000>>;
 sys_now_duration test = std::chrono::system_clock::duration::zero();
 
@@ -23,6 +23,6 @@ private:
 	Googly ball_;
 	Physics physics_;
 	Rocker seesaw_;
-	Stabilizer controller;
+	Stabilizer controller_;
 }; 
 
