da7ed5613bb840e86f529a5ac3888ffc45645772
waldhube16 <simon.waldhuber@edu.fh-joanneum.at>
2018-06-06 14:08:16 +0200

    minor changes

--- BalanceControl/BalanceLib/GameController.h
+++ BalanceControl/BalanceLib/GameController.h
@@ -15,11 +15,13 @@ class GameController
 public:
 	GameController();
 
+	void update_values();
+
 
 
 private:
 	Googly ball_;
-	Physics physics_ = Physics( 0,  test);
+	Physics physics_;
 	Rocker seesaw_;
 	Stabilizer controller;
 }; 
